c97ade3a6a31b335266e51e267f23e873bf964e5
rucka <miroslav.rucka@posam.sk>
2013-12-01 16:57:19 +0100

    connect to mongoDB

--- app/server.js
+++ app/server.js
@@ -1,14 +1,14 @@
 var express = require('express');
-var mongoose = require('mongoose/');
+var mongoose = require('mongoose');
 var config = require('./config');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 server.listen(config.port);
 
-console.info(config);
-
-
+/**
+ * configure express server
+ */
 app.use(express.logger('dev'));
 app.use('/js', express.static(__dirname + '/static/js'));
 app.use('/css', express.static(__dirname + '/static/css'));
@@ -18,13 +18,38 @@ app.use(app.router);
 app.engine('html', require('ejs').renderFile);
 
 
+/**
+ * serve root
+ */
 app.get('/', function (req, res) {
     res.render('index.html');
 });
 
+/**
+ * configure socket.io
+ */
 io.sockets.on('connection', function (socket) {
     socket.emit('news', { hello:'world' });
     socket.on('my other event', function (data) {
         console.log(data);
     });
 });
+
+var db = mongoose.connect('mongodb://localhost/chat');
+
+var chatSchema = mongoose.Schema({
+    username:String,
+    message:String
+})
+
+
+var Chat = mongoose.model('Chat', chatSchema);
+
+new Chat({username:'webstorm', message:'hi from nodejs!'}).save(function (err, chat) {
+
+});
+
+Chat.find(function (err, chats) {
+    console.info(chats);
+});
+
