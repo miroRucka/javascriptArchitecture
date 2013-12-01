59302ab2dc6e8dd70815ff27f3c9994a8e0481df
rucka <miroslav.rucka@posam.sk>
2013-12-01 15:52:52 +0100

    add favicon

--- app/server.js
+++ app/server.js
@@ -1,20 +1,25 @@
 var express = require('express');
+var mongoose = require('mongoose/');
+var config = require('./config');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
-server.listen(80);
+server.listen(config.port);
 
-app.configure(function () {
-    app.use(express.logger('dev'));
-    app.use('/js', express.static(__dirname + '/static/js'));
-    app.use('/css', express.static(__dirname + '/static/css'));
-    app.set('views',__dirname + '/views');
-    app.use(app.router);
-    app.engine('html', require('ejs').renderFile);
-});
+console.info(config);
+
+
+app.use(express.logger('dev'));
+app.use('/js', express.static(__dirname + '/static/js'));
+app.use('/css', express.static(__dirname + '/static/css'));
+app.use(express.favicon(__dirname + '/static/images/favicon.ico'));
+app.set('views', __dirname + '/views');
+app.use(app.router);
+app.engine('html', require('ejs').renderFile);
+
 
 app.get('/', function (req, res) {
-    res.render('test1.html');
+    res.render('index.html');
 });
 
 io.sockets.on('connection', function (socket) {
